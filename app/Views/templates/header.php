f50836387a69ac888efeae11dfc5370a5b0bf9e9
GaofengPan <1254066225@qq.com>
2020-10-07 10:01:48 +0800

    submit

---
+++ app/Views/templates/header.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+<head>
+    <title>CodeIgniter Tutorial</title>
+</head>
+<body>
+
+    <h1><?= esc($title); ?></h1>
+
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
